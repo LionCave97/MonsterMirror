4a97f034ed4e40847099a4aac6843bb476361650
Leo Kuyper <leokuyper@gmail.com>
2020-04-05 20:25:59 +0200

    Final

--- NodeJs/server.js
+++ NodeJs/server.js
@@ -1,9 +1,74 @@
-var fs = require('fs')
+var https = require('https'); //Https module of Node.js
+var fs = require('fs'); //FileSystem module of Node.js
+var FormData = require('form-data'); //Pretty multipart form maker.
+var folder = "../pics";
 
+var ACCESS_TOKEN = "";
 
-function startUp(){
+function Facebook(){
     console.log("Start up");
 
+    if (fs.existsSync(folder)){
+      fs.readdir(folder, (err, files) => {
+        files.forEach(file => {
+          console.log(file);
+
+          var form = new FormData(); //Create multipart form
+          form.append('file', fs.createReadStream(folder+'/'+file)); //Put file
+          form.append('message', "The Real Monster"); //Put message
+          
+          //POST request options, notice 'path' has access_token parameter
+          var options = {
+              method: 'post',
+              host: 'graph.facebook.com',
+              path: '/me/photos?access_token='+ACCESS_TOKEN,
+              headers: form.getHeaders(),
+          }
+          
+          //Do POST request, callback for response
+          var request = https.request(options, function (res){
+              console.log(res);
+          });
+          
+          //Binds form to request
+          form.pipe(request);
+          
+          //If anything goes wrong (request-wise not FB)
+          request.on('error', function (error) {
+              console.log(error);
+          });
+
+
+        });
+      });
+    }
+
+}
+
+function CopyPics(){
+
+  if (fs.existsSync(folder)){
+    fs.readdir(folder, (err, files) => {
+      files.forEach(file => {
+        console.log(file);
+
+        var cbCalled = false;
+
+        var rd = fs.createReadStream(folder + "/" +file);
+        rd.on("error", function(err) {
+        });
+        var wr = fs.createWriteStream("./pics/" + file);
+        wr.on("error", function(err) {
+        });
+        wr.on("close", function(ex) {
+        });
+        rd.pipe(wr);        
+        
+
+
+      });
+    });
+  }
 
 }
 
@@ -14,4 +79,7 @@ connect().use(serveStatic(__dirname)).listen(8080, function(){
 });
 
 
-startUp();
+CopyPics();
+Facebook();
+
+ 
